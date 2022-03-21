30d391e62a5ab4fcbbc42976f0b76fdde483f9f6
jacob harris <jacob.harris@prima.it>
2022-03-21 22:44:35 +0000

    Start merge

--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -1,7 +1,9 @@
 mod insertion_sort;
+mod merge_sort;
 mod selection_sort;
 mod shell_sort;
 
 pub use self::insertion_sort::insertion_sort;
+pub use self::merge_sort::merge_sort;
 pub use self::selection_sort::selection_sort;
 pub use self::shell_sort::shell_sort;
